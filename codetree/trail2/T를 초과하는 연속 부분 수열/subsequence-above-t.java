4ed91825207a719266ff449d5d46b9e1385b2073
ahyeon <52151533+dhflxhdxhd@users.noreply.github.com>
2026-09-22 14:00:10 +0900

    [Passed] T를 초과하는 연속 부분 수열 (136ms, 10MB)

--- codetree/trail2/T를 초과하는 연속 부분 수열/subsequence-above-t.java
+++ codetree/trail2/T를 초과하는 연속 부분 수열/subsequence-above-t.java
@@ -12,10 +12,8 @@ public class Main {
         for (int i = 0; i < n; i++){
             arr[i] = sc.nextInt();
             
-            if(i >= 1 &&  arr[i] > t){
+            if( arr[i] > t){
                 len++;
-            }else if(i == 0 && arr[i] > t){
-                len = 1;
             }else{
                 maxLen = Math.max(maxLen, len);
                 len = 0; 
